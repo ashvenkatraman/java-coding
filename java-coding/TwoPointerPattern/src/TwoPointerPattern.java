e549dbb6ac4273590bf516f4da813579ff427e28
ashvenkatraman <ashwin04@gmail.com>
2023-10-05 15:48:24 -0400

    Adding project code

---
+++ java-coding/TwoPointerPattern/src/TwoPointerPattern.java
@@ -0,0 +1,23 @@
+
+public class TwoPointerPattern {
+
+    public static boolean findSumOfThree(int[] nums, int target){
+
+        int left;
+        int right;
+
+
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        TwoPointerPattern tpp = new TwoPointerPattern();
+        boolean answer = TwoPointerPattern.findSumOfThree(new int[]{-1,0,1,2},4);
+        System.out.println("findSumOfThree: " + answer);
+
+    }
+
+
+
+}
